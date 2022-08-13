eccb85540f5d521f6f90cd82dd3ea7f6de6821ef
Kazuki Taninaka <onelive555@gmail.com>
2022-08-13 20:20:23 +0900

    adjust sytles

--- src/components/landing/About.tsx
+++ src/components/landing/About.tsx
@@ -1,4 +1,4 @@
-import styles from "../../styles/landing/About.module.css";
+import styles from "../../styles/landing/About.module.scss";
 
 const About = () => {
   return (
@@ -8,7 +8,8 @@ const About = () => {
         <li>@azoookid / Kazuki Taninaka</li>
         <li>Web frontend engineer based in Tokyo</li>
         <li>
-          Languages: TypeScript, JavaScript / Node.js, Golang, Japanese, English
+          Languages: TypeScript, JavaScript / Node.js, Golang, Japanese,
+          English(Conversational-level)
         </li>
         <li>Frameworks: React, Next.js, React Native</li>
       </ul>
--- src/components/landing/History.tsx
+++ src/components/landing/History.tsx
@@ -4,17 +4,29 @@ const History = () => {
   return (
     <div>
       <h2 className={styles.heading2}>⌛ History</h2>
-      <div className={styles.section}>
-        <h3>Internships</h3>
-        <ul>
-          <li>Kaminashi, Inc.</li>
-        </ul>
-      </div>
-      <div className={styles.section}>
-        <h3>Education</h3>
-        <ul>
-          <li>Kaminashi, Inc.</li>
-        </ul>
+      <div className={styles.sections}>
+        <div className={styles.section}>
+          <h3 className={styles.heading3}>Internships</h3>
+          <ul className={styles.list}>
+            <li>
+              Kaminashi, Inc.
+              <ul>
+                <li>Stacks: React, TypeScript, ReactNative, Golang</li>
+                <li>Duration: 7 months</li>
+              </ul>
+            </li>
+          </ul>
+          <p className={styles.paragraph}>
+            I have also joined short web dev internships at several companies
+            like CyberAgent, Cybozu, Rakuten etc...
+          </p>
+        </div>
+        <div className={styles.section}>
+          <h3 className={styles.heading3}>Education</h3>
+          <ul>
+            <li>Waseda University, School of International Liberal Studies</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
