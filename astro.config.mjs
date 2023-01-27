b0dc736e86a1706910053ec0ed4f0a6e0f6c4920
Kazuki Taninaka <onelive555@gmail.com>
2023-01-27 22:38:14 +0100

    add ga

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
-
+import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   site: "https://www.kazukitaninaka.github.io",
 });
